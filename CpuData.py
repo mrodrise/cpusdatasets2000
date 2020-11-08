c5bc435616c2d7710b166a57e2d386f874d60a02
mrodrise <mrodrise@gmail.com>
2020-11-08 18:44:03 +0100

    Nueva version con comentarios y parseado robots.txt

--- CpuData.py
+++ CpuData.py
@@ -4,16 +4,24 @@ from bs4 import BeautifulSoup
 import base64
 import os
 
+# Clase CpuData, dispone de los siguientes métodos:
+        # __attr_init__ --> Para inicializar las distintas propiedades del objeto
+        # __init__ --> Para inicializar el objeto, se apoya en el método __attr_init__
+        # store_data --> Se utiliza para para almacenar los datos de una CPU junto con sus imágenes
+        # __get_b64_img --> Se utiliza para pasar a base64 una imágen en caso de que la queramos guardar en el dataset
+        # __collect_data --> Se utiliza para recoger los datos de la página web, lo que permitirá luego almacenarlos
+        # __download_url --> Es el método que baja el html de la página web para que se pueda tratar
 
 class CpuData:
 
+    # Se utiliza para inicializar las distintas propiedades que tendrá el objeto
     def __attr_init__(self):
-        # Source
+        # Sería la url de donde sacamos la información
         self.url = ''
         self.plain_html = ''
         self.soup_html = None
 
-        # CPU Properties
+        # Los distintos datos que puede tener una CPU
         self.cpu_properties = {
             'cpu_name': '',
             'cpu_logo': '',
@@ -58,10 +66,12 @@ class CpuData:
             'chipset': ''
         }
 
-        # Table were the softare is going to look for info, based on table class at site
+        # Son las distintas tablas que contienen los datos de una CPU dentro de la página web
+        # Se deben cargar los datos de estas tablas para disponer de toda la información de la CPU
         self.standard_tables = ['Physical', 'Performance', 'Architecture', 'Cores', 'Cache']
 
-        # Dictionary to translate from site tables to our class CPU Properties
+        # Se trata de un diccionario que nos permite traducir los distintos campos de la página web de CPUs
+        # a los distintos campos dentro de la propiedad cpu_properties que hemos definido anteriormente
         self.enum_properties = {
             'Socket:': 'socket',
             'Foundry:': 'foundry',
@@ -101,26 +111,39 @@ class CpuData:
             'Chipset:': 'chipset'
         }
 
+    # Utiliza el método __attr_init__ para inicializar las distintas propiedades del objeto
+    # Además, lee la página web para la CPU de la que queremos descargar los datos
     def __init__(self, url):
+        # Inicializamos los atributos
         self.__attr_init__()
+        # En la propiedad url ponemos la url de la que queremos extraer la información
         self.url = urlparse(url)
+        # Llamamos al método __download_url para leer el código html de la página y
+        # pasarlo a la propiedad plain_html
         self.plain_html = self.__download_url(url)
+        # Utilizamos BeautifulSoup para parsear el código html
         self.soup_html = BeautifulSoup(self.plain_html, 'html.parser')
+        # Llamamos a la función que nos permite colectar los datos a partir del html ya parseado
         self.__collect_data()
 
-    # Method used to store CPU Data in disk
+    # Método utilizado para almacenar la información de CPU en el disco
     def store_data(self, path, include_headers, create_images, img_as_path):
         if include_headers and not os.path.isfile(path):
             headers = ",".join(self.cpu_properties.keys())
             with open(path, 'w') as f:
                 f.write(headers + "\n")
         data = ''
+
+        # Recorremos los distintos campos de la estructura cpu_properties
+        # Es donde van a almacenar los distintos datos de la página
         for key in self.cpu_properties:
             writed_data = False
+            # Si el campo es cpu_logo, guardamos el path donde vamos a guardar la imagen del logo
             if key == 'cpu_logo' and img_as_path:
                 data = data + '"' + 'img/' + self.cpu_properties['cpu_name'].replace(' ', '_') + '/' + \
                        self.cpu_properties['cpu_name'].replace(' ', '_') + '_logo.png' + '"' + ','
                 writed_data = True
+            # Si se ha pulsado la opción de guardar las imágenes, almacenamos el logo
             if key == 'cpu_logo' and create_images:
                 if self.cpu_properties['cpu_logo'] is not "":
                     if not os.path.isdir('img'):
@@ -130,6 +153,8 @@ class CpuData:
                     with open('img/' + self.cpu_properties['cpu_name'].replace(' ', '_') + '/' +
                               self.cpu_properties['cpu_name'].replace(' ', '_') + '_logo.png', "wb") as f:
                         f.write(base64.decodebytes(self.cpu_properties['cpu_logo']))
+            # Hacemos lo mismo con el resto de imágenes de la página web
+            # indicamos la ruta donde vamos a grabar las imágenes
             if key == 'imgs' and img_as_path:
                 imgs_paths = {}
                 for img in self.cpu_properties[key]:
@@ -138,6 +163,7 @@ class CpuData:
                                       img.replace(' ', '_') + '.png'
                 data = data + '"' + str(imgs_paths) + '"' + ','
                 writed_data = True
+            # Bajamos las imágenes a la ruta que se haya indicado
             if key == 'imgs' and create_images:
                 for img in self.cpu_properties[key]:
                     if not os.path.isdir('img'):
@@ -151,32 +177,48 @@ class CpuData:
             if not writed_data:
                 data = data + '"' + str(self.cpu_properties[key]) + '"' + ','
         data = data[0:-1]
+        # En cada vuelta del bucle, almacenamos la información en el fichero de datos
         with open(path, 'a') as f:
             f.write(data + "\n")
 
-    # Method to download and transform into base64 an image
+    # Método para descargar una imagen y transformarla a base64
     @staticmethod
     def __get_b64_img(url):
+        # Preparamos la url y la cabecera
         req = urllib.request.Request(
             url,
             data=None,
             headers={
-                'User-Agent': 'UOC Test Bot. Img downloader. lgarciatar@uoc.edu.'
+                'User-Agent': 'UOC Test Bot. Img downloader. lgarciatar@uoc.edu & mrodrise@uoc.edu'
             }
         )
 
+        # Descargamos la imagen
         img = urllib.request.urlopen(req, timeout=10).read()
 
+        # Transformamos la imagen a base64
         b64_img = base64.b64encode(img)
 
         return b64_img
 
-    # This method collects the data from the site and hydrates the properties of the object
+    # Este método recoge la información de la página web y rellena las propiedades del objeto
+    # Implica conocer cómo está estructurada la página web, en base a los distintos tags podrá
+    # recolecar los disintos datos cara a rellenar las propiedades del objeto
     def __collect_data(self):
+
+        # Nos vamos al punto del html donde podremos comenzar a recoger los datos
         article = self.soup_html.find('article')
+
+        # Creo una lista con todos los tags de tipo div
         article_div_list = article.find_all('div')
+
+        # Recorro la lista y voy recogiendo los distintos valores
         for div in article_div_list:
+            # Si el div tiene clases, revisamos las distintas clases ya que dentro estará la información
+
             if 'class' in div.attrs:
+
+                # Si tiene la clase clearfix y no tiene la clase images, estamos en la zona de nombre y logo
                 if 'clearfix' in div.attrs['class'] and 'images' not in div.attrs['class']:
                     for h1 in div.find_all('h1'):
                         if 'cpuname' in h1.attrs['class']:
@@ -186,6 +228,8 @@ class CpuData:
                             self.cpu_properties['cpu_logo'] = self.__get_b64_img('{uri.scheme}://{uri.netloc}'.
                                                                                  format(uri=self.url) +
                                                                                  img.attrs['src'])
+                # Si tiene la clase clearfix y tiene la clase images, estamos en la zona de imágenes
+                # Por tanto, procedemos a descargarlas si el usuario lo ha solicitado
                 if 'clearfix' in div.attrs['class'] and 'images' in div.attrs['class']:
                     for inner_div in div.findAll('div'):
                         if 'chip-image' in inner_div.attrs['class']:
@@ -196,6 +240,8 @@ class CpuData:
                                 img = self.__get_b64_img(src)
                             name = str(inner_div.find('div', {"class": "chip-image--type"}).string)
                             self.cpu_properties['imgs'][name] = img
+
+                # Se trataría de la zona donde están las tablas con la información de la CPU
                 if 'sectioncontainer' in div.attrs['class']:
                     for section in div.find_all('section'):
                         if str(section.find('h1').string) in self.standard_tables:
@@ -203,6 +249,9 @@ class CpuData:
                             for row in table.findAll('tr'):
                                 th = str(row.find('th').string)
                                 td = str(row.find('td').text)
+
+                                # Evaluamos el valor del campo de información, si no lo tenemos lo añadimos
+                                # al fichero no_enum para que el usuario disponga también de estos datos
                                 try:
                                     self.cpu_properties[self.enum_properties[th]] = td.strip().replace("\n", "")\
                                         .replace("\r", "")
@@ -216,18 +265,20 @@ class CpuData:
                             text_container = section.find('td', {"class": "p"})
                             self.cpu_properties['notes'] = str(text_container.string).strip().replace("\n", "")
 
-    # This method is the one that downloads the HTML from the page.
+    # Este método se encarga de descargar el código html de la página web
     @staticmethod
     def __download_url(url):
-        # Descargamos contenido de la url
+        # Ponemos la cabecera e indicamos la url de la que vamos a descargar el código
         req = urllib.request.Request(
             url,
             data=None,
             headers={
-                'User-Agent': 'UOC Test Bot. lgarciatar@uoc.edu.'
+                'User-Agent': 'UOC Test Bot. lgarciatar@uoc.edu & mrodrise@uoc.edu'
             }
         )
 
+        # Hacemos la petición de descarga del código de la url
         html_doc = urllib.request.urlopen(req, timeout=10).read()
 
+        # Devolvemos el código que hemos descargado
         return html_doc
--- main.py
+++ main.py
@@ -5,20 +5,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 import time
 import platform
+import sys
+# importamos robotparser que tiene utilidades para leer el fichero robots.txt
+from urllib import robotparser
+
+# Definimos en otro fichero la clase CpuData que a partir de una URL de una CPU,
+# extraiga todos sus datos e imágenes, se utilizará más adelante en el bucle principal de extracción
+# de los datos de las distintas CPUs.
 from CpuData import CpuData
 
 
 def driver_config():
 
-    # If there's any option to configure add here.
+    # En caso de que se quieran añadir opciones se puede hacer aquí
     chrome_options = Options()
-    # Adding headless mode.
+
+    # Una opción para evitar que no nos dejen hacer webscraping es no usar cabeceras
     chrome_options.add_argument("--headless")
-    # Detecting current OS to use the correct driver path.
+
+    # Antes de iniciar el webdriver necesitamos saber si el sistema operativo es Linux, Mac o Windows
+    # El path del ejecutable cambia para windows con respecto a Linux y Mac ya que necesita extensión .exe
     os = platform.system()
     print("Detected OS:", os)
 
-    # Start chromedriver.
+    # Iniciamos en webdriver dependiendo del sistema operativo
     if os == "Windows":
         inner_driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=chrome_options)
     else:
@@ -28,65 +38,100 @@ def driver_config():
 
 
 def get_info_cpus(driver_inner, year_inner):
-    # Added delay to prevent 429 errors, and webscrapping being detected.
+    # La página web detecta web scraping, bloqueando si detecta que se hacen muchas peticiones seguidas
+    # Vamos a introducir un delay para evitar que nos bloqueen
     t0 = time.time()
-    # Loading current year page.
+    # Cargamos la página web para el año que nos han pasado
     driver_inner.get(f"https://www.techpowerup.com/cpu-specs/?released={year_inner}&sort=name")
-    # Waiting to required web element to load.
+    # Esperamos a que cargue la página hasta el elemento que necesitamos
     try:
         WebDriverWait(driver_inner, 5).until(lambda s: s.find_element_by_class_name("processors").is_displayed())
     except TimeoutException:
         print("ERROR - TimeoutException: Webpage can't be loaded.")
         return None
 
-    # Getting response delay
+    # Vemos cuánto es el delay entre que lanzamos la petición y tenemos el resultado
     response_delay = time.time() - t0
 
-    # Creating the soup using BeautifulSoup
+    # Utilizamos BeautifulSoup para tratar con más facilidad la informaicón cargada
     soup_inner = BeautifulSoup(driver.page_source, "lxml")
 
-    # Variable used to count the number of CPUs in a year.
+    # Utilizamos la variable j para tener un contador a modo de resumen de número de procesadores para ese año
     j = 0
 
-    # Searching for the CPU table trying to find the link to detail page.
+    # Buscamos la tabla processors donde está el listado de procesadores con sus links a las páginas de detalle
     for table in soup_inner.select("table.processors"):
         for detail_link in table.select("a"):
-            # print(detail_link['href'])
+            print(detail_link['href'])
+
+            # Creamos un objeto de tipo CpuData y lo inicializamos con la URL de la que extraer la información
             cpu = CpuData('https://www.techpowerup.com'+detail_link['href'])
+
+            # Almacenamos los datos que hemos extraido de esa url
             cpu.store_data(dataset_path, include_headers, create_images, img_as_path)
+
             j += 1
-            # Waiting for the delay to avoid 429 codes.
+            # Tenemos que poner tiempos de espera ya que el servidor detecta acciones muy seguidas desde una misma IP
+            # Estos tiempos de espera se han establecido por prueba y error, en base a las distintas ejecuciones realiadas
             time.sleep(2*60)
+
+            # Cada 100 extracciones debemos meter un tiempo de espera prudencial para que el servidor no nos bloquee
             if j % 100 == 0:
                 time.sleep(5*60)
 
     print("A total of: ", j, " links where found.")
 
-    # Waiting for the delay to avoid 429 codes.
+    # Procedemos a meter un tiempo de espera prudencial en base al tiempo de respuesta del servidor.
+    # Así evitamos errores de tipo 423
     time.sleep(10 * response_delay)
 
+# *****************************************************
+# *************** PARTE MAIN DEL CÓDIGO ***************
+# *****************************************************
 
 if __name__ == '__main__':
 
-    # Creating driver object
+    # Antes de comenzar necesitamos saber si el administrador del sitio da permiso para bajar los datos
+    # Utilizamos un parser que nos ayude a leer el fichero robots.txt
+    rp = robotparser.RobotFileParser()
+
+    # Establecemos como url donde se encuentra el fichero robots.txt
+    rp.set_url("https://www.techpowerup.com/robots.txt")
+
+    # Leemos el fichero robots.txt
+    rp.read()
+
+    # Utilizando el parser vemos si para cualquier tipo de agente está permitido bajarse los datos
+    # En el momento en que se hizo este código se podía bajar la página que nos interesa para
+    # cualquier tipo de agente menos para SentiBot
+    if (rp.can_fetch("*", "https://www.techpowerup.com/cpu-specs/?sort=name") == False):
+        # Si el fichero robots.txt indica que no podemos bajar esa página, entonces finalizamos el programa
+        print("The administrator of the site doesn't allow to download this data, the program will finish")
+        sys.exit()
+
+    # Creamos el objeto de driver y lo inicializamos
     driver = driver_config()
 
-    # Opening main CPU webpage there's where we are going to find the data.
+    # Accedemos a la página principal donde está la información que queremos extraer
     driver.get(f"https://www.techpowerup.com/cpu-specs/?sort=name")
 
-    # Waiting for years selector to load.
+    # Esperamos a que cargue el elemento que necesitamos, en este caso es la tabla processors que contine
+    # la información de procesadores que necesitamos extraer
+
     try:
         WebDriverWait(driver, 5).until(lambda s: s.find_element_by_class_name("processors").is_displayed())
     except TimeoutException:
         print("ERROR - TimeoutException: Webpage can't be loaded.")
 
-    # Creating the soup using BeautifulSoup
+    # Utilizamos BeautifulSoup con el parser que parece más extendido: lxml
     soup = BeautifulSoup(driver.page_source, "lxml")
 
-    # Getting years selector
+    # Buscamos el elemento que contiend el id "released"
+    # El objetivo es leer de este selector todos los años para los que la página web tiene información
     course_page = soup.find(id='released')
 
-    # Loading possible years.
+    # Cargamos todos los años, buscando el tag option dentro del selector
+    # Ignoramos el primer valor de option ya que es para cargar los resultados más populares y queremos años
     years = []
     i = 0
     for year_selector in course_page.find_all("option"):
@@ -97,48 +142,69 @@ if __name__ == '__main__':
 
     print("Years ", years)
 
-    # Ask for options and selected years
+    # Preguntamos al usuario en qué carpeta quiere guardar la información.
+    # Si pulsa D o pulsa enter, entonces creará el dataset en el directorio actual
+
     dataset_path = input("Default path (\"D\") or custom:")
     if dataset_path == "D" or len(dataset_path) < 2:
         dataset_path = 'dataset.csv'
+
+    # Se le pregunta al usuario si quiere que las imágenes las guardemos en una carpeta o si prefiere
+    # que las guardemos en un campo de dataset en formato bs4.
     img_as_path = input("Img as path in dataset (\"Y\") or as BS4 (\"N\") (BS4 could lead to broken lines in dataset):")
     if img_as_path == 'N':
         img_as_path = False
     else:
         img_as_path = True
+
+    # Preguntamos al usuario si quiere que generemos las imágenes o no
     create_images = input("Generate images (\"Y\"/\"N\"):")
     if create_images == "N":
         create_images = False
     else:
         create_images = True
+
+    # Preguntamos al usuario si quiere que se incluyan headers
     include_headers = input("Include headers (\"Y\"/\"N\"):")
     if include_headers == "N":
         include_headers = False
     else:
         include_headers = True
+
+    # Preguntamos al usuario qué años quiere que extraigamos de la página web
+    # Si pulsa enter, se bajará todos los años
     selected_years = input("As default dataset will contain all the loaded years, if you prefer to pick just some "
                            "years write them separated by coma for example (2010,2011,2012):")
+
+    # selected_years es un string, miramos que el usuario al menos haya introducido 2 caracteres
     if len(selected_years) > 2:
         selected_years_list = selected_years.split(',')
         selected_years_list_clean = []
+        # Revisamos que los años introducidos estén dentro del rango de años de la lista de años de la página web
         for year in selected_years_list:
             if year in years and year not in selected_years_list_clean:
                 selected_years_list_clean.append(year)
             else:
                 print('Year '+year+' has been removed, incorrect input or not available.')
+        # Si se comprueba después de hacer limpieza que no hay años válidos, se cargan todos
         if len(selected_years_list_clean) < 1:
             print('Not even 1 year was available, all years will be downloaded.')
             selected_years = None
     else:
+        # En caso de que lo haya dejado vacío o bien introducido 2 caracteres o menos, entendemos
+        # que se quieren descargar todos los años
         selected_years = None
 
-    # Getting all info from all CPUs in all years.
+    # Ya tenemos todos los años que se quieren extraer por parte del usuario
+    # Recorremos la lista de años obtenemos la información que necesitamos por año
     for year in years:
         print("***************************")
         print("Year to load: ", year)
         print("***************************")
         search_keyword = year
+
+        # Esta es la función que hemos creado para extraer la información por año
         get_info_cpus(driver, year)
 
-    # Close the driver.
+    # Cerramos el driver que habíamos creado
     driver.close()
